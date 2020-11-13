eae9fa5e8d6c97ed00eccc59858ec989ba84a766
adebayoileri <46798106+adebayoileri@users.noreply.github.com>
2020-11-13 19:29:28 +0100

    memoji

--- src/Pages/About/index.tsx
+++ src/Pages/About/index.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {
     Box,
     BoxProps,
-    // chakra,
-    // Text,
+    chakra,
+    Text,
+    // Link,
+    Flex,
+    Image
 } from "@chakra-ui/core";
 import Header from "../../components/Header";
 
@@ -18,7 +21,30 @@ export default function About() {
             <Box mb={20}>
                 <Box as="section" pt="12rem" pb="6rem">
                     <Container>
-                        I
+                        <Box maxW="760px" mx="auto" textAlign="center">
+                           
+                           <Flex align="center" justify="center" >
+                                <Image
+                                borderRadius="full"
+                                boxSize="180px"
+                                src="https://res.cloudinary.com/adebayo/image/upload/v1605201224/86B7FA45-3753-4936-9F64-DAFE4C092ECE_czsef2.png"
+                                alt="Adebayo Ilerioluwa"
+                                />
+                                </Flex>
+                            <chakra.h5
+                                fontSize="5xl"
+                                letterSpacing="tight"
+                                fontWeight="bold"
+                                mb="16px"
+                                lineHeight="1.2"
+                            >
+                                I'm Adebayo Ilerioluwa
+                            </chakra.h5>
+
+                            <Text fontSize={{ base: "lg", lg: "xl" }} mt="2">
+                                Nice to meet you! <span aria-label="smile" role="img">😁</span>
+                            </Text>
+                        </Box>
                     </Container>
                 </Box>
             </Box>
