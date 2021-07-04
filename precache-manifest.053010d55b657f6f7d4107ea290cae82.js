29414f67760b50602b8fd5c16e9fa8b0451965e8
Adebayo Ilerioluwa <adebayorilerioluwa@gmail.com>
2021-07-04 18:02:56 +0100

    Updates

---
+++ precache-manifest.053010d55b657f6f7d4107ea290cae82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe7af0672a10e280a5047fe3a5e489e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d34cb873283bb30a2a1e",
+    "url": "/static/css/main.ea6522b6.chunk.css"
+  },
+  {
+    "revision": "e25581c34fdfd295a3de",
+    "url": "/static/js/2.69995f45.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.69995f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fdf9557865bda60b9b6",
+    "url": "/static/js/3.22b55595.chunk.js"
+  },
+  {
+    "revision": "d34cb873283bb30a2a1e",
+    "url": "/static/js/main.8a675692.chunk.js"
+  },
+  {
+    "revision": "e3a4708911c89a713a39",
+    "url": "/static/js/runtime-main.0a0fa14a.js"
+  }
+]);
